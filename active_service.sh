50df484df14b9b96447d69937401f597de8dafcf
Elias Müller <elias.muller1@gmail.com>
2021-05-20 15:59:25 -0300

    ajust

--- active_service.sh
+++ active_service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "[Unit]" > /etc/systemd/system/485_temperature.service
-echo "Description=485 Service \n" >> /etc/systemd/system/485_temperature.service
-echo "Wants=network.target \n" >> /etc/systemd/system/485_temperature.service
-echo "After=network.target \n" >> /etc/systemd/system/485_temperature.service
-echo "[Service]\n" >> /etc/systemd/system/485_temperature.service
-echo "ExecStartPre=/bin/sleep 10\n" >> /etc/systemd/system/485_temperature.service
-echo "ExecStart=/home/pi/python_485_network/run.py\n" >> /etc/systemd/system/485_temperature.service
-echo "Restart=always\n" >> /etc/systemd/system/485_temperature.service
+echo "Description=485 Service" >> /etc/systemd/system/485_temperature.service
+echo "Wants=network.target" >> /etc/systemd/system/485_temperature.service
+echo "After=network.target" >> /etc/systemd/system/485_temperature.service
+echo "[Service]" >> /etc/systemd/system/485_temperature.service
+echo "ExecStartPre=/bin/sleep 10" >> /etc/systemd/system/485_temperature.service
+echo "ExecStart=/home/pi/python_485_network/run.py" >> /etc/systemd/system/485_temperature.service
+echo "Restart=always" >> /etc/systemd/system/485_temperature.service
 echo "[Install]" >> /etc/systemd/system/485_temperature.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/485_temperature.service
 
