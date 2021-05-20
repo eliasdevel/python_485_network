2602e79f02a558f7fa27e226f971176bd51779ca
Elias Müller <elias.muller1@gmail.com>
2021-05-20 15:53:13 -0300

    ajust

--- active_service.sh
+++ active_service.sh
@@ -1,12 +1,12 @@
-[Unit]
-Description=485 Service
-Wants=network.target
-After=network.target
+#!/bin/bash
 
-[Service]
-ExecStartPre=/bin/sleep 10
-ExecStart=/home/pi/python_485_network/run.py
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+echo "[Unit]" > /etc/systemd/system/485_temperature.service
+echo "Description=485 Service \n" >> /etc/systemd/system/485_temperature.service
+echo "Wants=network.target \n" >> /etc/systemd/system/485_temperature.service
+echo "After=network.target \n" >> /etc/systemd/system/485_temperature.service
+echo "[Service]\n" >> /etc/systemd/system/485_temperature.service
+echo "ExecStartPre=/bin/sleep 10\n" >> /etc/systemd/system/485_temperature.service
+echo "ExecStart=/home/pi/python_485_network/run.py\n" >> /etc/systemd/system/485_temperature.service
+echo "Restart=always\n" >> /etc/systemd/system/485_temperature.service
+echo "[Install]" >> /etc/systemd/system/485_temperature.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/485_temperature.service
