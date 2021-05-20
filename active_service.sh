c9a5c684b5da21b8257b642397d2f1712a4df521
Elias Müller <elias.muller1@gmail.com>
2021-05-20 16:24:01 -0300

    ajust

--- active_service.sh
+++ active_service.sh
@@ -8,9 +8,11 @@ echo "[Service]" >> /etc/systemd/system/485_temperature.service
 echo "Type=simple" >> /etc/systemd/system/485_temperature.service
 echo "Restart=always" >> /etc/systemd/system/485_temperature.service
 echo "RestartSec=3" >> /etc/systemd/system/485_temperature.service
-echo "ExecStart=/home/pi/python_485_network/run.sh" >> /etc/systemd/system/485_temperature.service
+echo "User=pi" >> /etc/systemd/system/485_temperature.service
+echo "ExecStart=/usr/bin/env python /home/pi/python_485_network/485_service.py" >> /etc/systemd/system/485_temperature.service
 echo "[Install]" >> /etc/systemd/system/485_temperature.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/485_temperature.service
+
 
 sudo systemctl disable 485_temperature.service
 
